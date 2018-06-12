0e60b39cc9297a486d311e1060e7f76feb6af098
tdreid <trevor.d.reid@gmail.com>
2018-06-12 03:46:00 +0000

    Initialize router for resources

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+const resources = require('./routes/resources');
 
 var app = express();
 
