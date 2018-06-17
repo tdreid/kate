8b384e94f53a0d8f29f28183ac1f5a249085687d
tdreid <trevor.d.reid@gmail.com>
2018-06-17 21:53:54 +0000

    POST to create a new resource

--- routes/resources.js
+++ routes/resources.js
@@ -5,20 +5,31 @@ const Resource = require('../models/resourceModel');
 
 mongoose.connect('mongodb://localhost/resources');
 
-router.route('/Resources').get((req, res, next) => {
-  Resource.find(req.query, (err, resources) => {
-    if (err) {
-     next(err);
-    } else {
-      res.json(resources);
+router
+  .route('/Resources')
+  .get((req, res, next) => {
+    Resource.find(req.query, (err, resources) => {
+      if (err) {
+        next(err);
+      } else {
+        res.json(resources);
+      }
+    });
+  })
+  .post((req, res, next) => {
+    try {
+      const resource = new Resource(req.body);
+      resource.save();
+      res.status(201).json(resource);
+    } catch (err) {
+      next(err);
     }
   });
-});
 
 router.route('/Resources/:id').get((req, res, next) => {
   Resource.findById(req.params.id, (err, resources) => {
     if (err) {
-     next(err);
+      next(err);
     } else {
       res.json(resources);
     }
